f885d629751b007c3ef1100b748d1669713d2618
algernon-A <aeneas@aloku.net>
2021-04-16 22:11:41 +1000

    Prepare for 0.9.1 release.

--- Code/Mod.cs
+++ Code/Mod.cs
@@ -11,7 +11,7 @@ namespace ABLC
     public class ABLCMod : IUserMod
     {
         public static string ModName => "Advanced Building Level Control";
-        public static string Version => "0.9";
+        public static string Version => "0.9.1";
 
         public string Name => ModName + " " + Version;
         public string Description => Translations.Translate("ABLC_DESC");
